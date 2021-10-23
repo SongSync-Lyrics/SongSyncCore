e75c55b3bcac6809df9d09c115c2d8bc92594b58
tokanada <davidt.3200@gmail.com>
2021-10-23 18:25:32 -0400

    Add rooms function

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,82 @@
 // Initialize Socket.IO
-let socket = io();
+let socket = io()
+
+let room
+let form = document.getElementById('form')
+let input = document.getElementById('input')
+
 
 // References to HTML file
-const startingSection = document.querySelector('.starting-section');
-const homeBtn = document.querySelector('.home-btn');
-let crazyButton = document.getElementById('crazyButton');
+const startingSection = document.querySelector('.starting-section')
+const homeBtn = document.querySelector('.home-btn')
+const crazyButton = document.getElementById('crazyButton')
+const leaderLabel = document.getElementById('leader')
+const followerLabel = document.getElementById('follower')
+
 
 // When startButton is clicked, send response to server
-startButton.addEventListener('click', () => {
-    socket.emit('startGame');
+//startButton.addEventListener('click', () => {
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    if (input.value) {
+        room = input.value
+        socket.emit('startGame', input.value)
+        console.log("Joining room: " + input.value)
+    }
 })
 
-// When socket.io receives 'startGame' response from server, run hideStartButton
-socket.on('startGame', () => {
-    hideStartButton();
+homeBtn.addEventListener('click', () => {
+    socket.emit('listConnectedUsers')
 })
-
-// Function that hides startButton, displays redSquare
-function hideStartButton() {
-    startButton.style.display = "none";
-    crazyButton.style.display = "block";
-    startingSection.style.display = "none";
-}
 
 // When redSquare is clicked, send response to server containing a randomized movement command with it. Movement command is based on screen size
 crazyButton.addEventListener('click', () => {
     socket.emit('crazyIsClicked', {
         offsetLeft: Math.random() * ((window.innerWidth - crazyButton.clientWidth) - 100),
         offsetTop: Math.random() * ((window.innerHeight - crazyButton.clientHeight) - 50)
-    });
+    }, room)
+})
+
+
+// When socket.io receives 'startGame' response from server, run hideStartButton
+socket.on('startGame', () => {
+    hideStartButton()
+    socket.emit('leaderStatus', room)
+})
+
+socket.on('leaderStatus', (leaderStatus) => {
+    if(leaderStatus){
+        leaderLabel.style.display = "block"
+    } else {
+        followerLabel.style.display = "block"
+    }
+})
+
+socket.on('leaderDisconnect', () => {
+    followerLabel.innerHTML = "Leader has disconnected"
 })
 
 // When socket.io receives 'crazyIsClicked' response from server, run goCrazy. Data contains values given from first client response
 socket.on('crazyIsClicked', (data) => {
-    goCrazy(data.offsetLeft, data.offsetTop);
-});
+    goCrazy(data.offsetLeft, data.offsetTop)
+})
 
 // Based on values given, set new redSquare location
 function goCrazy(offLeft, offTop) {
-    let top, left;
+    let top, left
 
-    left = offLeft;
-    top = offTop;
+    left = offLeft
+    top = offTop
 
-    crazyButton.style.top = top + 'px';
-    crazyButton.style.left = left + 'px';
-    crazyButton.style.animation = "none";
+    crazyButton.style.top = top + 'px'
+    crazyButton.style.left = left + 'px'
+    crazyButton.style.animation = "none"
+}
+
+// Function that hides startButton, displays redSquare
+function hideStartButton() {
+    //startButton.style.display = "none"
+    input.value = ''
+    form.style.display = "none"
+    crazyButton.style.display = "block"
 }
--- server/server.js
+++ server/server.js
@@ -1,44 +1,148 @@
 // Initialization of backend
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketIO = require('socket.io')
 
 // Path to public folder
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, '/../public')
 // Port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+const roomMap = new Map()
 
 // Initialization of Express.JS
-let app = express();
-let server = http.createServer(app);
+let app = express()
+let server = http.createServer(app)
 // Initialization of Socket.IO Server
-let io = socketIO(server);
+let io = socketIO(server)
 
 // Let Express.JS know of public path
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
 
 // Start server on port
 server.listen(port, ()=> {
-    console.log('Server is up on port ${port}.')
-});
+    console.log('Server is up on port ' + port +'.')
+})
 
 // Start Socket.IO connection with clients
 io.on('connection', (socket) => {
+    function roomMapHasRoom(room) {
+        return roomMap.has(room)
+    }
+
+    function isRoomEmpty(room) {
+        const arr = Array.from(io.sockets.adapter.rooms)
+        const filtered = arr.filter(room => !room[1].has(room[0]))
+        // ==> ['room1', 'room2']
+        const rooms = filtered.map(i => i[0])
+        return !rooms.includes(room)
+    }
+
+    function removeEmptyRooms() {
+        roomMap.forEach((values, key) => {
+            if (isRoomEmpty(key)) {
+                console.log("removeEmptyRooms| Empty Room Found " + key)
+                roomMap.delete(key)
+                console.log("removeEmptyRooms| Empty Room Removed " + key)
+            }
+        })
+    }
+
+    function isLeaderAction(socketid, room) {
+        let leader = roomMap.get(room)[1]
+
+        return leader == socketid
+    }
+
+    async function checkLeaderDisconnect() {
+        const sockets = await io.fetchSockets()
+        let leaderMissing = true
+
+        for (const socket of sockets){
+            roomMap.forEach((values, key) => {
+                if (isLeaderAction(socket.id, key)) {
+                    leaderMissing = false
+                }
+            })
+        }
+
+        if (leaderMissing) {
+            console.log("checkLeaderDisconnect| Leader Disconnected!")
+            io.emit('leaderDisconnect')
+        }
+    }
+
+    function getActiveRooms(io) {
+        // Convert map into 2D list:
+        // ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
+        const arr = Array.from(io.sockets.adapter.rooms)
+
+        // Filter rooms whose name exist in set:
+        // ==> [['room1', Set(2)], ['room2', Set(2)]]
+        const filtered = arr.filter(room => !room[1].has(room[0]))
+
+        return filtered
+    }
+
     // Message on user join and exit
-    console.log("A user just connected");
+    console.log("\nconnection| A user just connected")
+
     socket.on('disconnect', () => {
-        console.log("A user has disconnected");
+        console.log("\ndisconnect| A user has disconnected")
+        removeEmptyRooms()
+        checkLeaderDisconnect()
     })
 
     // Action when client clicks startButton. Flow: Client Press -> Server Receive -> Server Response -> All Client Action
-    socket.on('startGame', () => {
-        io.emit('startGame');
+    socket.on('startGame', (room) => {
+        if (!roomMapHasRoom(room)) {
+            console.log("\nstartGame| New Room Detected")
+            let posAndLeader = [undefined, socket.id]
+            roomMap.set(room, posAndLeader)
+            socket.join(room)
+            console.log("startGame| Leader " + socket.id + " joining room " + room)
+            io.to(room).emit('startGame')
+        } else {
+            console.log("\nstartGame| Follower joining existing room")
+            socket.join(room)
+            console.log("startGame| Follower " + socket.id + " joining room " + room)
+            io.to(room).emit('startGame')
+            if (roomMap.get(room)[0] != undefined) {
+                io.to(room).emit('crazyIsClicked', roomMap.get(room)[0])
+            }
+        }
+        checkLeaderDisconnect()
     })
 
     // Action when client clicks RedSquare
-    socket.on('crazyIsClicked', (data) => {
-        io.emit('crazyIsClicked', data);
-    });
-});
+    socket.on('crazyIsClicked', (data, room) => {
+        let posAndLeader = roomMap.get(room)
+        let leader = posAndLeader[1]
+        posAndLeader = [data, leader]
+
+        roomMap.set(room, posAndLeader)
+
+        if (isLeaderAction(socket.id, room)) {
+            console.log("\ncrazyIsClicked| Leader Action Accepted")
+            io.to(room).emit('crazyIsClicked', data)
+        } else {
+            console.log("\ncrazyIsClicked| Not Leader, Action Rejected")
+        }
+
+    })
+    
+    socket.on('listConnectedUsers', () => {
+        console.log("\nlistConnectedUsers| User " + socket.id + " requested list of users")
+        console.log("listConnectedUsers| List of users and rooms: ")
+
+        console.log(getActiveRooms(io))
+        console.log(roomMap)
+    })
+
+    socket.on('leaderStatus', (room) => {        
+        io.to(socket.id).emit('leaderStatus', isLeaderAction(socket.id, room))
+    })
+
+})
+
 
